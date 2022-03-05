007a23923c96e7a0f469c668fb72fe392d2656a8
Nworm <1574242600@qq.com>
2022-03-05 16:40:23 +0800

    feat: 更换 wgcf 客户端为 cf-warp

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,24 @@
 FROM ubuntu:20.04
 
-
-RUN apt-get update && apt-get install -y \
-  curl ca-certificates \
-  iproute2 net-tools iptables \
-  wireguard-tools openresolv  kmod --no-install-recommends \
-  && rm -rf /var/lib/apt/lists/*
-  
-  
-RUN curl -fsSL git.io/wgcf.sh | bash && mkdir -p /wgcf
-
-WORKDIR /wgcf
-
-VOLUME /wgcf
-
+ENV WARP_VERSION="2022.2.29-1"
+ENV WARP_DEB_URL="https://pkg.cloudflareclient.com/pool/dists/focal/main/cloudflare_warp_2022_2_29_1_amd64_4c914fa5af_amd64.deb"
+ENV WARP_DEB_SHA256="d690f6345ce378cce25991144ab471ac3276aa11ffe64ec1fb25de1c94a2bf97"
 
 COPY entry.sh /entry.sh
-RUN chmod +x /entry.sh
 
-ENTRYPOINT ["/entry.sh"]
+RUN apt-get update \
+  && apt-get install -y \
+  ca-certificates curl \
+  iproute2 net-tools \
+  # wrap dependencies
+  systemctl libdbus-1-3 libc6 nftables gnupg2 \
+  && cd /tmp \
+  && curl $WARP_DEB_URL -o warp.deb \
+  && echo "$WARP_DEB_SHA256  warp.deb" | sha256sum -c - \
+  && dpkg -i warp.deb \
+  && rm -rf /var/lib/apt/lists/* \
+  && rm -rf /tmp/*
 
+VOLUME /var/lib/cloudflare-warp
 
+ENTRYPOINT ["/bin/bash", "/entry.sh"]
--- entry.sh
+++ entry.sh
@@ -2,96 +2,97 @@
 
 set -e
 
+DEFAULT_GATEWAY_NETWORK_CARD_NAME=`route  | grep default  | awk '{print $8}' | head -1`
+DEFAULT_ROUTE_IP=`ifconfig $DEFAULT_GATEWAY_NETWORK_CARD_NAME | grep "inet " | awk '{print $2}' | sed "s/addr://"`
+
+#-4|-6
+runwgcf() {
+  trap '_downwgcf' ERR TERM INT
+  systemctl start warp-svc
+
+  WARP_PATH="/var/lib/cloudflare-warp"
+
+
+
+
+  if [ ! -e "$WARP_PATH/reg.json" ]; then
+    echo "y" | warp-cli register
+    warp-cli set-mode warp
+  else 
+    echo "y" | warp-cli rotate-keys
+  fi 
+
+
+  case $1 in
+    "-4")
+        [[ $(warp-cli get-excluded-routes) =~ "0.0.0.0/0" ]] && warp-cli remove-excluded-route 0.0.0.0/0
+        warp-cli add-excluded-route 0::/0
+        ;;
+    "-6")
+        [[ $(warp-cli get-excluded-routes) =~ "0::/0" ]] && warp-cli remove-excluded-route 0::/0
+        warp-cli add-excluded-route 0.0.0.0/0
+        ;;
+  esac
+
+  warp-cli connect && ip rule add from $DEFAULT_ROUTE_IP lookup main
+  
+  echo 
+  case $1 in
+    "-4")
+      _checkV4
+      ;;
+    "-6")
+      _checkV6
+      ;;
+    *)
+      echo "ipv4: "
+      _checkV4
+      echo 
+      echo "ipv6: "
+      _checkV6
+      ;;
+  esac
+
+
+  echo 
+  echo "OK, cf-warp is up."
+  
+  sleep infinity & wait
+}
+
 
 _downwgcf() {
   echo
   echo "clean up"
-  if ! wg-quick down wgcf; then
+  if ! warp-cli disconnect; then
     echo "error down"
   fi
+  ip rule delete from $DEFAULT_ROUTE_IP lookup main
   echo "clean up done"
   exit 0
 }
 
 
 
-#-4|-6
-runwgcf() {
-  trap '_downwgcf' ERR TERM INT
-
-  _enableV4="1"
-  if [ "$1" = "-6" ]; then
-    _enableV4=""
-  fi
-
-
-  if [ ! -e "wgcf-account.toml" ]; then
-    wgcf register --accept-tos
-  fi
-
-  if [ ! -e "wgcf-profile.conf" ]; then
-    wgcf generate
-  fi
-  
-  cp wgcf-profile.conf /etc/wireguard/wgcf.conf
-
-  DEFAULT_GATEWAY_NETWORK_CARD_NAME=`route  | grep default  | awk '{print $8}' | head -1`
-  DEFAULT_ROUTE_IP=`ifconfig $DEFAULT_GATEWAY_NETWORK_CARD_NAME | grep "inet " | awk '{print $2}' | sed "s/addr://"`
-  
-  echo ${DEFAULT_GATEWAY_NETWORK_CARD_NAME}
-  echo ${DEFAULT_ROUTE_IP}
-  
-  sed -i "/\[Interface\]/a PostDown = ip rule delete from $DEFAULT_ROUTE_IP  lookup main" /etc/wireguard/wgcf.conf
-  sed -i "/\[Interface\]/a PostUp = ip rule add from $DEFAULT_ROUTE_IP lookup main" /etc/wireguard/wgcf.conf
-
-  if [ "$1" = "-6" ]; then
-    sed -i 's/AllowedIPs = 0.0.0.0/#AllowedIPs = 0.0.0.0/' /etc/wireguard/wgcf.conf
-  elif [ "$1" = "-4" ]; then
-    sed -i 's/AllowedIPs = ::/#AllowedIPs = ::/' /etc/wireguard/wgcf.conf
-  fi
-
-
-  modprobe ip6table_raw
-  
-  wg-quick up wgcf
-  
-  if [ "$_enableV4" ]; then
-    _checkV4
-  else
-    _checkV6
-  fi
-
-  echo 
-  echo "OK, wgcf is up."
-  
-
-  sleep infinity & wait
-  
-  
-}
 
 _checkV4() {
   echo "Checking network status, please wait...."
-  while ! curl --max-time 2  ipinfo.io; do
-    wg-quick down wgcf
+  while ! curl --max-time 2  https://www.cloudflare.com/cdn-cgi/trace/; do
+    warp-cli disconnect
     echo "Sleep 2 and retry again."
     sleep 2
-    wg-quick up wgcf
+    warp-cli connect
   done
-
-
 }
 
 _checkV6() {
   echo "Checking network status, please wait...."
-  while ! curl --max-time 2 -6 ipv6.google.com; do
-    wg-quick down wgcf
+  while ! curl --max-time 2 -6 https://www.cloudflare.com/cdn-cgi/trace/; do
+    warp-cli disconnect
     echo "Sleep 2 and retry again."
     sleep 2
-    wg-quick up wgcf
+    warp-cli connect
   done
-
-
 }
 
 
@@ -101,5 +102,3 @@ if [ -z "$@" ] || [[ "$1" = -* ]]; then
 else
   exec "$@"
 fi
-
-
